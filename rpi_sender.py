986ec2adca201b5beaa0e8825856106e439bb224
pmonda <praneymonda@gmail.com>
2025-05-19 09:48:55 -0400

    added server side code

--- rpi_sender.py
+++ rpi_sender.py
@@ -1,31 +1,16 @@
-import cv2
 import socket
-from split_model_utils import *
+import time
 
-# Setup
-model = YOLO('yolov8n.pt')
-model = attach_hooks(model)
-
-pc_ip = '192.168.1.205' 
-port = 5005
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Load test image
-img = cv2.imread('test.jpg')
-img = cv2.resize(img, (640, 640))
-img_tensor = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).float() / 255
-
-# Run part of the model
-_ = model(img_tensor)
-
-# Extract features
-tensor = intermediate_outputs["conv2"]
-data, shape = compress_tensor(tensor)
-
-# Send shape info
-sock.sendto(str(shape).encode(), (pc_ip, port))
-# Send compressed data in chunks
-for i in range(0, len(data), 4096):
-    sock.sendto(data[i:i+4096], (pc_ip, port))
-
-print("Sent tensor with shape", shape)
+bufferSize = 1024
+msgFromServer = "Hello Client"
+serverPort = 2222
+serverIP = "192.168.1.204"
+bytesToSend = msgFromServer.encode('utf-8')
+RPIsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+RPIsocket.bind((serverIP,serverPort))
+print('Server is Listening ...')
+message, address = RPIsocket.recvfrom(bufferSize)
+message = message.decode('utf-8')
+print(message)
+print('Client Address ', address[0])
+RPIsocket.sendto(bytesToSend, address)
