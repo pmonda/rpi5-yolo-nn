5af68ccea2ffa0eae8807d85beeebba12cffb8e0
pmonda <praneymonda@gmail.com>
2025-05-20 18:58:41 -0400

    sender/receiver files for image transfer

--- src/receiver.py
+++ src/receiver.py
@@ -1,19 +1,28 @@
 import socket
-
-msgFromClient = "Hello Server from Client"
-bytesToSend = msgFromClient.encode('utf-8')
+import time
+# msgFromClient = "Hello Server from Client"
+# bytesToSend = msgFromClient.encode('utf-8')
 serverAddress = ('192.168.1.204', 2222) #RPI address
 bufferSize = 1024
 UDPClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
-    cmd = input("Send Message: ")
-    # cmd = input('What do you want to do with counter, INC or DEC? ')
-    cmd = cmd.encode('utf-8')
-    UDPClient.sendto(cmd, serverAddress)
-    data, address = UDPClient.recvfrom(bufferSize)
-    data = data.decode('utf-8')
-    print(f"Port {address[1]}@{address[0]} says: {data}")
-    if "bye" in data.lower():
-        print("Server says bye, closing client")
-        UDPClient.close()
-        break
+    f = open("pmh2mmc4.png", "rb")
+    data = f.read(bufferSize)
+    while(data):
+        if (UDPClient.sendto(data, serverAddress)):
+            data = f.read(bufferSize)
+    
+    f.close()
+    UDPClient.sendto("end image", serverAddress)
+    time.sleep(0.2)
+    # cmd = input("Send Message: ")
+    # # cmd = input('What do you want to do with counter, INC or DEC? ')
+    # cmd = cmd.encode('utf-8')
+    # UDPClient.sendto(cmd, serverAddress)
+    # data, address = UDPClient.recvfrom(bufferSize)
+    # data = data.decode('utf-8')
+    # print(f"Port {address[1]}@{address[0]} says: {data}")
+    # if "bye" in data.lower():
+    #     print("Server says bye, closing client")
+    #     UDPClient.close()
+    #     break
--- src/sender.py
+++ src/sender.py
@@ -1,31 +1,46 @@
 import socket
 import time
-
+from PIL import Image
 bufferSize = 1024
 msgFromServer = "Hello Client"
 serverPort = 2222
 serverIP = "192.168.1.204"
-bytesToSend = msgFromServer.encode('utf-8')
+# bytesToSend = msgFromServer.encode('utf-8')
 RPIsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 RPIsocket.bind((serverIP,serverPort))
+addr = (serverIP, serverPort)
 print('Server is Listening ...')
-cnt = 0
+# cnt = 0
 while True:
-    message, address = RPIsocket.recvfrom(bufferSize)
-    message = message.decode('utf-8')
-    print(f"Port {address[1]}@{address[0]} says: {message}")
-
-    if message == 'INC':
-        cnt += 1
-    if message == 'DEC':
-        cnt -= 1
+    bytes = 0
+    f = open("pmh2mmc4.png", "wb")
+    data, addr = RPIsocket.recvfrom(bufferSize)
+    while(data):
+        f.write(data)
+        RPIsocket.settimeout(20)
+        bytes += len(data)
+        data, adddr = RPIsocket.recvfrom(bufferSize)
+        if data == "end image":
+            f.close()
+            Image.open("pmh2mmc4.png").show()
+            time.sleep(20)
+            break
     
-    msg_orig = input("Send Message: ")
-    msg = msg_orig.encode('utf-8')
-    RPIsocket.sendto(msg, address)
-    if "bye" in msg_orig.lower():
-        print("Client says bye, exiting...")
-        RPIsocket.close()
-        break
+    # message, address = RPIsocket.recvfrom(bufferSize)
+    # message = message.decode('utf-8')
+    # print(f"Port {address[1]}@{address[0]} says: {message}")
+
+    # if message == 'INC':
+    #     cnt += 1
+    # if message == 'DEC':
+    #     cnt -= 1
+    
+    # msg_orig = input("Send Message: ")
+    # msg = msg_orig.encode('utf-8')
+    # RPIsocket.sendto(msg, address)
+    # if "bye" in msg_orig.lower():
+    #     print("Client says bye, exiting...")
+    #     RPIsocket.close()
+    #     break
     
     
