6f23bd9fc0da2ffa727e118e60edd7d0baaf79a6
pmonda <praneymonda@gmail.com>
2025-06-13 08:57:18 -0400

    added seq num

--- src/client.py
+++ src/client.py
@@ -1,16 +1,21 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('192.168.1.205', 1003)
+server_address = ('192.168.1.205', 1003)  # Replace with PC's IP
 
 with open("pmh2mmc4.png", "rb") as file:
+    seq = 0
     while True:
-        chunk = file.read(65000)
+        chunk = file.read(4096 - 4)  # 4 bytes for sequence number
         if not chunk:
             break
-        client.sendto(chunk, server_address)
+        # Prepend sequence number (4 bytes, big-endian)
+        packet = seq.to_bytes(4, byteorder='big') + chunk
+        client.sendto(packet, server_address)
+        seq += 1
 
-# Optionally send an empty packet to signal EOF
-client.sendto(b'EOF', server_address)
+# Send EOF packet with sequence number -1 (0xFFFFFFFF)
+client.sendto((-1).to_bytes(4, byteorder='big', signed=True), server_address)
 
 client.close()
+print("Image sent.")
--- src/server.py
+++ src/server.py
@@ -3,11 +3,24 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(('0.0.0.0', 1003))
 
-with open("output.jpg", "wb") as file:
-    while True:
-        data, addr = server.recvfrom(65000)
-        if data == b'EOF':  # end-of-file signal
-            break
-        file.write(data)
+received_chunks = {}
+
+print("Waiting for image data...")
+
+while True:
+    data, addr = server.recvfrom(4096)
+    seq = int.from_bytes(data[:4], byteorder='big', signed=True)
+
+    if seq == -1:
+        print("EOF received.")
+        break
+
+    received_chunks[seq] = data[4:]  # Strip the sequence header
+
+# Write all chunks in order to a file
+with open("output.jpg", "wb") as f:
+    for i in sorted(received_chunks.keys()):
+        f.write(received_chunks[i])
 
 server.close()
+print("Image reassembled and saved.")
